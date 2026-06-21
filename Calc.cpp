4e3d67267c441a56aa0ceb62975c328fc834efbc
jllanquev <jllanquev@unjbg.edu.pe>
2026-06-21 18:11:20 -0500

    First time

---
+++ Calc.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int  opc;
+	do
+	{
+		cout<<"\t\t\t1. Sumar"<<endl;
+		cout<<"\t\t\t2. Restar"<<endl;
+		cout<<"\t\t\t3. Multiplicar"<<endl;
+		cout<<"\t\t\t4. Dividir"<<endl;
+		cout<<"\t\t\t5. Salir"<<endl;
+		cout<<"\n\t\t\tIngrese su operacion: ";cin>>opc;
+		
+		switch (opc)
+		{
+			case 1: 
+				system ("cls");
+				cout<<"\t\t\tMultiplicacion\n";
+			case 2:
+			case 3:
+			case 4:
+				break;
+		}
+	} while (opc > 0 && opc < 5);
+	
+	return 0;
+}
